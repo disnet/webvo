0eac79204c3377933f3ac5dd050232d00478feb0
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-16 22:31:55 +0000

    git-svn-id: http://webvo.googlecode.com/svn@24 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/form_schedule_with_libxml.rb
+++ trunk/ruby/form_schedule_with_libxml.rb
@@ -71,40 +71,68 @@ def get_header(xml)
   output_string ="<?xml version=\"" + xml.version.to_s 
   output_string << "\" encoding=\"" 
   output_string << xml.encoding.to_s + "\"?>" + "\n"
-  output_string << xml.doctype.to_s + "\n\n"
   return output_string
 end
 
-def shows(xml, start_date_time, end_date_time)
-  puts "got here"
-  output = File.open("out.xml","a+")
-  puts start = (start_date_time[8..9] + start_date_time[10..11] + start_date_time[12..15]).to_i
-  puts start_date = start_date_time[0..3] + start_date_time[4..5] + start_date_time[6..7]
-  end_time = (end_date_time[8..9] + end_date_time[10..11] + start_date_time[12..15]).to_i
-  puts end_date = end_date_time[0..3] + end_date_time[4..5] + end_date_time[6..7]
-  puts (unformat_date(end_date)-unformat_date(start_date)).to_i
-  
-  if start_date != end_date:
-    puts stop = end_time + 240000*(unformat_date(end_date)-unformat_date(start_date)).to_i
-  else
-    stop = end_time
-  end
+# Main-----------------------------------------------------------------------------------
+puts( "Starting " + DateTime.now.to_s)
+#checking for three arguements
+  error_if_not_equal(ARGV.length, 2, "Needs two arguments")
 
+
+#get arguments
+  puts start_date_time = ARGV[0]
+  puts end_date_time = ARGV[1]
+
+#checks lengths of arguements to make sure the have the length of YYYYMMDDHHMMSS
+  error_if_not_equal(start_date_time.length, LENGTH_OF_DATE_TIME, "incorrect length for start date")
+  error_if_not_equal(end_date_time.length, LENGTH_OF_DATE_TIME, "incorrect length for end date")
+  
+  start_date = start_date_time[0..7]
+  end_time = end_date_time[8..13]
+  start_time = start_date_time[8..13]
+  end_date = end_date_time[0..7]
+
+#error checking
+  #Check if date stamp valid
+  error_if_not_equal(start_date_time.to_i < end_date_time.to_i, true, "Start time must be before end time")
+
+  #Check if times are valid
+  error_if_not_equal(start_time.to_i < 240000, true, "Start time must be millitary time")
+  error_if_not_equal(end_time.to_i < 240000, true, "End time must be millitary time") 
+  error_if_not_equal(start_time[2..3].to_i < 60 , true, "Start minutes must be less than 60")
+  error_if_not_equal(end_time[2..3].to_i < 60, true, "End minutes must be less than 60")
+  
+  #Check if dates are valid
+  error_if_not_equal(start_date[4..5].to_i <= 12 , true, "Starting month must be less than or equal to 12")
+  error_if_not_equal(end_date[4..5].to_i <= 12 , true, "Ending month must be less than or equal to 12")
+  error_if_not_equal(start_date[6..7].to_i <= 31, true, "Starting day must be less than 31")
+  
+  error_if_not_equal(end_date[6..7].to_i <= 31, true, "Ending day must be less than 31")
+  
+  #Get output the information into output
+  puts "Parsing"
+  xml = XML::Document.file("info.xml")
+  to_client_xml = XML::Document.new()
+
+  start = start_date_time.to_i
+  stop = end_date_time.to_i
+
+  first_time = true
+  newest_e = 0
+
+  output = "la" #puts get_header(xml)
   
   xml.find("programme").each do |e|
     #get element start and end time
-    e_start_date = e['start'][0..7]
-    e_end_date = e['stop'][0..7]
+    puts e_start = e['start'][0..13].to_i
+    puts e_end = e['stop'][0..13].to_i
     
-    next if e_start_date.to_i < start_date.to_i
+    #get newest element
+    if e_end > newest_e:
+      newest_e = e_end 
+    end
 
-    e_start = e['start'][8..15].to_i
-    e_end = e['stop'][8..15].to_i
-    
-    if e_start_date != e_end_date:
-      e_end = e_end + 240000*(unformat_date(e_end_date)-unformat_date(e_start_date)).to_i
-    end 
-    
     if e_start < e_end:
       begins_before = e_end > start && e_end <= stop && e_start <= stop && e_start < start
       ends_after = e_start >= start && e_end > stop && e_start < stop
@@ -114,58 +142,17 @@ def shows(xml, start_date_time, end_date_time)
       if begins_before || ends_after || occurs_during || occurs_around:
         STDOUT << "*"
         output << e.to_s
-        output << "\n"
       end
     else
       error_if_not_equal(false, true, "Have to deal with later")
     end
   end
-  output.close()
-end
+  
+  error_if_not_equal(stop <= newest_e, true, "Ran off end of info.xml")
 
-# Main-----------------------------------------------------------------------------------
-#checking for three arguements
-puts DateTime.now
-error_if_not_equal(ARGV.length, 2, "Needs two arguments")
-
-
-#get arguments
-  puts start_date_time = ARGV[0]
-  puts end_date_time = ARGV[1]
-
-  error_if_not_equal(start_date_time.length, LENGTH_OF_DATE_TIME, "incorrect length for start date")
-  error_if_not_equal(end_date_time.length, LENGTH_OF_DATE_TIME, "incorrect length for start date")
+  output.close()    
   
-  puts start_time = start_date_time[8..9] + start_date_time[10..11] + start_date_time[12..15]
-  puts start_date = start_date_time[0..3] + start_date_time[4..5] + start_date_time[6..7]
-  puts end_time = end_date_time[8..9] + end_date_time[10..11] + start_date_time[12..15]
-  puts end_date = end_date_time[0..3] + end_date_time[4..5] + end_date_time[6..7]
-#error checking
-  #check format of start_date
-  today = DateTime.now
-  
-  formatted_today = format_date(today)
-  
-  
-  #check format of start_time
-  error_if_not_equal(start_time.length, LENGTH_OF_TIME, "incorrect length for start time")
-  
-  #check format of end_time
-  error_if_not_equal(end_time.length, LENGTH_OF_TIME, "incorrect length for end time")
-  
-  #see if start_date is available
-  error_if_not_equal(file_available(start_date+".xml"), true, "start date is not available")
-  error_if_not_equal(file_available("channel"+".xml"), true, "channel is not avaiable")
-  
-  #Get output the information into output
-  #File.delete("out.xml")
-
-    puts "parsing"
-    puts DateTime.now.to_s
-    xml = XML::Document.file("info.xml")
-    puts( "Done Parsing " + DateTime.now.to_s)
-    shows(xml, start_date_time, end_date_time)
-    puts( "Done writing shows " + DateTime.now.to_s)
+  puts( "Done writing shows " + DateTime.now.to_s)
 
 
 
