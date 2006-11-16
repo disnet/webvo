e7ca13ee718158623029f51e39c45c8713414ef7
tim.disney <tim.disney@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-16 08:40:45 +0000

    git-svn-id: http://webvo.googlecode.com/svn@20 7c433a8e-ac21-0410-9248-3fa6fae40c50

---
+++ trunk/ruby/cgi_test.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+require 'cgi'
+cgi = CGI.new
+
+# If this is an html doc should be 'text/html'
+# If an xml doc then 'text/xml'
+puts "Content-Type: text/plain\n\n"
+
+puts "Hello, world"
+puts cgi['name']
