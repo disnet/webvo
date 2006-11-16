c2367628e91d6212c7c3687715b889629caa117b
tim.disney <tim.disney@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-16 09:15:20 +0000

    git-svn-id: http://webvo.googlecode.com/svn@22 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/cgi_test.rb
+++ trunk/ruby/cgi_test.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/ruby1.8
-require 'cgi'
-cgi = CGI.new
+#require 'cgi'
 
-# If this is an html doc should be 'text/html'
-# If an xml doc then 'text/xml'
-puts "Content-Type: text/plain\n\n"
+puts "Content-Type: text/plain\n\n"    # If this was an html doc the Content-Type would have been 'text/html' and if it was an xml doc it would be 'text/xml'
+                                       # It is always needed to help the browser decide what to do with the document
+                                       # It is a header and never seen in the output
 
-puts "Hello, world"
-puts cgi['name']
+cgi = CGI.new                                  # The CGI object is how we get the arguments
+puts "The name argument was #{cgi['name']}"
+puts "The job argument was #{cgi['job']}"
