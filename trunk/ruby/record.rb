2050208e8702e442389d862070ea1767cbe28b94
dsbruin <dsbruin@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-16 02:03:53 +0000

    git-svn-id: http://webvo.googlecode.com/svn@19 7c433a8e-ac21-0410-9248-3fa6fae40c50

---
+++ trunk/ruby/record.rb
@@ -0,0 +1,46 @@
+#Daryl Siu
+#Write to the recorder in Ruby
+
+#class to hold pertinent data for recording a show
+class RecordedShow
+  attr_accessor :channel
+  attr_accessor :starttime
+  attr_accessor :stoptime
+  attr_accessor :show
+
+  def inputVals(sid,chnl,statime,stotime)
+    @showID = sid
+    @channel = chnl
+    @starttime = statime
+    @stoptime = stotime
+  end
+  
+end
+
+#begin recording script
+
+if __FILE__ == $0
+  show = RecordedShow.new(20061028100,13,100,200)
+  
+  #calculate when recording show starts  
+  puts "Recording channel #{show.channel} from #{show.starttime} to #{show.stoptime}."
+  
+  #tune the card to the correct channel
+  commandSent = System ("ivtv -tune -c #{show.channel}")
+  if commandSent != true
+    puts "Channel set failed\n"
+  end
+  
+  #start the recording
+  commandSent = System ("cat /dev/video0 > #{show.showID}.mpg")
+  CAT_PID = $! #keeps track of last job ID number
+  if commandSent != true
+    puts "Recording failed to start\n"
+  end
+  #sleep for length of the show
+  sleep (60)
+
+  #stop the recording
+  kill CAT_PID if CAT_PID.alive? = true
+  
+end
