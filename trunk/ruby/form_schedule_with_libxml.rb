8bc252a8d645f1f9ba236b1a89ed7ab8620b8a85
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-17 00:14:55 +0000

    git-svn-id: http://webvo.googlecode.com/svn@26 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/form_schedule_with_libxml.rb
+++ trunk/ruby/form_schedule_with_libxml.rb
@@ -17,12 +17,16 @@
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 ################################################################################
 
-
+!/usr/bin/ruby1.8
+require 'cgi'
 require 'xml/libxml'
 require 'date'
 
+
 LENGTH_OF_TIME = 6
 LENGTH_OF_DATE_TIME = 14
+START = "start_date_time"
+STOP = "end_date_time"
 
 #Functions-------------------------------------------------------------------------------
 #make sure file available in current directory
@@ -31,62 +35,41 @@ def file_available(file_name)
   return cur_dir_entries.include?(file_name)
 end
 
-#formate_date formates a date time object and converts it to a string
-# formated YYYYMMDD
-def format_date(current_date)
-  month = current_date.month.to_s
-  day = current_date.day.to_s
-  
-  #pads month
-  if (current_date.month.to_s.length != 2):
-    month = "0"+ current_date.month.to_s
-  else
-    month = current_date.month.to_s
-  end
-  
-  #pads day
-  if (current_date.day.to_s.length != 2):
-    day = "0" + current_date.day.to_s
-  else
-    day = current_date.day.to_s
-  end
-  return current_date.year.to_s + month + day
-end
-
-#Turns string formatted YYYYMMDD to date time object
-def unformat_date(formatted_date)
-  return DateTime.new(formatted_date[0..3].to_i, formatted_date[4..5].to_i, formatted_date[6..7].to_i)
-end
-
 #Error handler
 def error_if_not_equal(value, standard, error_string)
   if value != standard:
-    puts("Error " + error_string)
+    puts "<error>Error " + error_string +"<\error>"
     exit
   end
 end
 
 
-def get_header(xml)
+#def get_header(xml)
   output_string ="<?xml version=\"" + xml.version.to_s 
   output_string << "\" encoding=\"" 
   output_string << xml.encoding.to_s + "\"?>" + "\n"
   return output_string
-end
+#end
 
 # Main-----------------------------------------------------------------------------------
-puts( "Starting " + DateTime.now.to_s)
-#checking for three arguements
-  error_if_not_equal(ARGV.length, 2, "Needs two arguments")
 
+
+  puts "Content-Type: text/xml\n\n" 
+  cgi = CGI.new                      # The CGI object is how we get the arguments 
+
+#checks for 2 arguments
+  error_if_not_equal(cgi.keys.length, 2, "Needs two arguments")
+
+  error_if_not_equal(cgi.has_key?(START), true, "Need start date time")
+  error_if_not_equal(cgi.has_key?(STOP), true, "Need end date time")
 
 #get arguments
-  puts start_date_time = ARGV[0]
-  puts end_date_time = ARGV[1]
+  start_date_time = cgi[START]
+  end_date_time = cgi[STOP]
 
-#checks lengths of arguements to make sure the have the length of YYYYMMDDHHMMSS
-  error_if_not_equal(start_date_time.length, LENGTH_OF_DATE_TIME, "incorrect length for start date")
-  error_if_not_equal(end_date_time.length, LENGTH_OF_DATE_TIME, "incorrect length for end date")
+#checks lengths of arguments to make sure the have the length of YYYYMMDDHHMMSS
+  error_if_not_equal(start_date_time.length, LENGTH_OF_DATE_TIME, "incorrect len for start date")
+  error_if_not_equal(end_date_time.length, LENGTH_OF_DATE_TIME, "incorrect len for end date")
   
   start_date = start_date_time[0..7]
   end_time = end_date_time[8..13]
@@ -104,55 +87,60 @@ puts( "Starting " + DateTime.now.to_s)
   error_if_not_equal(end_time[2..3].to_i < 60, true, "End minutes must be less than 60")
   
   #Check if dates are valid
-  error_if_not_equal(start_date[4..5].to_i <= 12 , true, "Starting month must be less than or equal to 12")
-  error_if_not_equal(end_date[4..5].to_i <= 12 , true, "Ending month must be less than or equal to 12")
-  error_if_not_equal(start_date[6..7].to_i <= 31, true, "Starting day must be less than 31")
-  
+  error_if_not_equal(start_date[4..5].to_i <= 12 , true, "Starting month must be <= to 12")
+  error_if_not_equal(end_date[4..5].to_i <= 12 , true, "Ending month error < 12")
+  error_if_not_equal(start_date[6..7].to_i <= 31, true, "Starting month error < 31") 
   error_if_not_equal(end_date[6..7].to_i <= 31, true, "Ending day must be less than 31")
   
   #Get output the information into output
-  puts "Parsing"
   xml = XML::Document.file("info.xml")
-  to_client_xml = XML::Document.new()
 
   start = start_date_time.to_i
   stop = end_date_time.to_i
 
   first_time = true
   newest_e = 0
-
-  output = "la" #puts get_header(xml)
+  oldest_e = 999999999999999
   
+  puts '<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<!DOCTYPE tv SYSTEM \"xmltv.dtd\">\n<tv source-info-url=\"http://labs.zap2it.com/\" source-info-name=\"TMS Data Direct Service\" generator-info-name=\"XMLTV\" generator-info-url=\"http://www.xmltv.org/\">'
+  
+  #go through file and get files within requested range 
   xml.find("programme").each do |e|
     #get element start and end time
-    puts e_start = e['start'][0..13].to_i
-    puts e_end = e['stop'][0..13].to_i
+    e_start = e['start'][0..13].to_i
+    e_end = e['stop'][0..13].to_i
     
-    #get newest element
+    #calculations for seeing if requesting unavailable timeframe
+      #get newest element
     if e_end > newest_e:
       newest_e = e_end 
     end
+    
+      #get oldest element
+    if e_start < oldest_e:
+      oldest_e = e_start
+    end
 
-    if e_start < e_end:
+    if e_start <= e_end:
+      #possible locations of programmes
       begins_before = e_end > start && e_end <= stop && e_start <= stop && e_start < start
       ends_after = e_start >= start && e_end > stop && e_start < stop
       occurs_during = e_start >= start && e_end <= stop
       occurs_around = (e_start <= start && e_end >= stop)
       
       if begins_before || ends_after || occurs_during || occurs_around:
-        STDOUT << "*"
-        output << e.to_s
+        puts e.to_s
       end
     else
-      error_if_not_equal(false, true, "Have to deal with later")
+      error_if_not_equal(false, true, "Invaild xml data")
     end
   end
+  puts "</tv>"
   
+#checking to see if the user requested an unavailable timeframe
+  error_if_not_equal(start >= oldest_e, true, "Ran off begining of info.xml")
   error_if_not_equal(stop <= newest_e, true, "Ran off end of info.xml")
-
-  output.close()    
   
-  puts( "Done writing shows " + DateTime.now.to_s)
 
 
 
