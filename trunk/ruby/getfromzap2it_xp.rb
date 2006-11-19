9dec7565efd587ac05c4d9da304c7c61ab797063
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-19 04:20:35 +0000

    git-svn-id: http://webvo.googlecode.com/svn@103 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/getfromzap2it_xp.rb
+++ trunk/ruby/getfromzap2it_xp.rb
@@ -100,8 +100,7 @@ logfile.close()
     xmldoc.find('channel').each do |e|	  
       chan_id = e["id"].to_s
       chan_desc = e.child
-      puts chan_desc.to_s
-      chan_number = chan_desc.content
+      puts chan_number = chan_desc.find('display-name').first.content.to_i
       #send to database
       #check if exists already
       if  chan_array.include?(chan_id) :
