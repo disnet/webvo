d7d507919b7fbb35574349403646fd164561743d
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-19 03:26:08 +0000

    git-svn-id: http://webvo.googlecode.com/svn@81 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/getfromzap2it_xp.rb
+++ trunk/ruby/getfromzap2it_xp.rb
@@ -1,3 +1,4 @@
+#!/usr/local/bin/ruby
 ################################################################################
 #WebVo: Web-based PVR
 #Copyright (C) 2006 Molly Jo Bault, Tim Disney, Daryl Siu
@@ -23,6 +24,14 @@
 
 
 require 'date'
+require 'mysql'
+require 'xml/libxml'
+
+SERVERNAME = "localhost"
+USERNAME = "root"
+USERPASS = "csc4150"
+DBNAME = "WebVo"
+TABLENAME = "Recording"
 
 #opening/creating log file
 logfile = File.new("logfile.txt", "a")
@@ -56,17 +65,63 @@ else
   
 end
 
-
-
-#parsing_xml_ran = system("ruby parsing_xml.rb")
-
-#if parsing_xml_ran == true:
-#  logfile << "Parsing_xml ran successfully at " + DateTime.now + "\n"
-  
-#else
-#  logfile << "Parsing_xml ran unsuccessfully.\n"
-#end
-
-
 logfile << "\n\n"
 logfile.close()
+
+#populating channels in database
+  xmldoc = XML::Document.file("info.xml")
+
+  #connect to database
+  begin
+  dbh = Mysql.real_connect("#{SERVERNAME}","#{USERNAME}","#{USERPASS}","#{DBNAME}")
+#  if get an error (can't connect)
+  rescue MysqlError => e
+      error_if_not_equal(false,true, "Error code: " + e.errno + "\n")
+      error_if_not_equal(false,true, "Error message: "+ e.error + "\n")
+    puts "Unable to connect to database\n"
+    if dbh.nil? == false
+      #close the database
+      dbh.close() 
+    end
+  else
+  #get channel_id and number and send to database
+  #This code will compare four cases
+  #1. If a new channel has been added it will update the database
+  #2. If a channel has been removed it will check programme to see 
+  #   if it is in use and if not it will delete it
+  #set up an array of all of the channel IDs to see if a channel has been taken off the air
+    chan_array = Array.new(0)
+    db_channelIDs = dbh.query("SELECT ChannelID FROM channel")
+    if db_channelIDs != nil:
+      channelIDs.each do |ci|
+        chan_array << ci[0]
+      end
+    end
+    xmldoc.find('channel').each do |e|	  
+      chan_id = e["id"].to_s
+      chan_number = e.child.content.to_i.to_s
+      #send to database
+      #check if exists already
+      if  chan_array.include?(chan_id) :
+        #delete from array
+        chan_id.delete(chan_id)
+      else
+      #if it doesn't exist add it to the database
+        dbh.query("INSERT INTO channel (ChannelID, number) VALUES (#{chan_id}, #{chan_number})")
+      end
+    end
+    
+    #go through removed channels and see if in use by programme, if so leave it 
+    #otherwise delete
+    chan_array.each do |ci|
+      db_prog_using_chan = dbh.query("SELECT ChannelID FROM programme WHERE ChannelID=(#{ci})")
+      if db_prog_using_chan == nil:
+        #if no programmes using that channel then delete it from channel
+        puts "deleting channel with ID " + ci
+        dbh.query("DELETE FROM channel WHERE ChannelID=(#{ci})")
+      else
+        puts ci + " still in use!"
+      end
+    end
+    
+  end 
