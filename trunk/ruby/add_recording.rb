b6d0f5a7b69c65335609608c2928326819f610eb
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-19 00:14:49 +0000

    git-svn-id: http://webvo.googlecode.com/svn@48 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/add_recording.rb
+++ trunk/ruby/add_recording.rb
@@ -1,3 +1,4 @@
+#!/usr/local/bin/ruby
 ################################################################################
 #WebVo: Web-based PVR
 #Copyright (C) 2006 Molly Jo Bault, Tim Disney, Daryl Siu
@@ -19,16 +20,20 @@
 #add_recording.rb
 #takes a programmeid from the front end and adds it to the database
 
-#!/usr/local/bin/ruby
-require 'cgi'
+#require 'cgi'
 require 'xml/libxml'
 require 'date'
+require "mysql"
+
+SERVERNAME = "localhost"
+USERNAME = "root"
+USERPASS = "csc4150"
+DBNAME = "WebVo"
+TABLENAME = "Recording"
 
 PROG_ID = "prog_id"
 LENGTH_OF_DATE_TIME = 14
 XML_FILE_NAME = "info.xml"
-
-
 
 def file_available(file_name)
   cur_dir_entries=Dir.entries(Dir.getwd)
@@ -43,7 +48,6 @@ def error_if_not_equal(value, standard, error_string)
 end
 
 #main--------------------------------------------------------------------------
-
   puts "Content-Type: text/xml\n\n" 
   
   #cgi = CGI.new     # The CGI object is how we get the arguments 
@@ -53,7 +57,7 @@ end
 
   #error_if_not_equal(cgi.has_key?(PROG_ID), true, "Need Programme ID")
 #get argument
-  prog_id = "11111111120061206080000" #cgi[PROG_ID][0]
+  prog_id = "I12500.labs.zap2it.com20061119080000" #cgi[PROG_ID][0]
 
   error_if_not_equal(prog_id.length > LENGTH_OF_DATE_TIME, true, "Needs a Channel ID")
   
@@ -75,8 +79,79 @@ end
 #get programme from info.xml
   error_if_not_equal(file_available(XML_FILE_NAME), true, XML_FILE_NAME +"not in directory")
   xml = XML::Document.file(XML_FILE_NAME)
+  got_programme = true
   
-  xml.find("programme") do |e|
-    if xml["channel"] == chan_id &&
+  xml.find("programme").each do |e|
+    if e["channel"] == chan_id && e["start"][0..LENGTH_OF_DATE_TIME-1] == date_time)
+      #get channel id, start time, stop time, title, and all xml information
+      #channel id -> chan_id
+      #start time -> start
+      #stop time -> stop
+      #title -> title (note: all ' ' spaces converted to '_' underscores
+      #xml information -> xmlNode
+      
+      error_if_not_equal(got_programme, true, "two or more programmes match that program ID")
+      
+      xmlNode = e.copy(true).to_s
+      start = e["start"][0..LENGTH_OF_DATE_TIME-1]
+      stop = e["stop"][0..LENGTH_OF_DATE_TIME-1]
+      
+      error_if_not_equal(e.child?, true, "programme to add doesn't have needed information")
+      c = e.child
+      need_title = true
+      keep_looping = true
+      
+      #gets the title
+      while need_title == true && keep_looping == true
+        if c.name == "title":
+          title = (c.content).gsub(/[' ']/, '_')
+          need_title = false
+        end  
+        if c.next?:
+            c = c.next
+        else
+          keep_looping = false
+        end
+      end
+      error_if_not_equal(need_title, false, "programme doesn't have a title")
+      got_programme = false
+    end
+  end
+  
+  #connect to database
+  begin
+  dbh = Mysql.real_connect("#{SERVERNAME}","#{USERNAME}","#{USERPASS}","#{DBNAME}")
+  #if get an error (can't connect)
+  rescue MysqlError => e
+      error_if_not_equal(false,true, "Error code: " + e.errno + "\n")
+      error_if_not_equal(false,true, "Error message: "+ e.error + "\n")
+    puts "Unable to connect to database\n"
+    if dbh.nil? == false
+      #close the database
+      dbh.close() 
+    end
+  else
+    #add the programme to the database
+    #check and make sure that the programme isn't already there
+    results = dbh.query("SELECT * FROM Recording WHERE (channelID = #{chan_id} AND start = #{start})")
+    
+    #send information to programme's table
+      #send channel ID
+      #database = chan_id
+      #database = start
+      #database = stop
+      #database = title
+      #database = xmlNode
+      
+    #check and make sure that programme isn't in recording table already
+      #send information to recording table
+      #database = channel ID
+      #database = start
+      
+    #close the database
+    dbh.close()
+  end
 
-  
+    
+  #call record.rb
+  system("ruby record.rb")
