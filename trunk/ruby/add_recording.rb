1516bf92527006471049cf2bfa91cb744b6acc7a
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-27 05:49:56 +0000

    git-svn-id: http://webvo.googlecode.com/svn@215 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/add_recording.rb
+++ trunk/ruby/add_recording.rb
@@ -228,8 +228,8 @@ end
       error_if_not_equal(true, false, "channel from requested show not in database")
     end
     puts start + " " + stop + " " + title + " "
-    channel_info.each_hash do |row|
-      puts row["number"].to_s
+    channel_info.each do |row|
+      puts row[0].to_s
     end
     puts chan_id + " " + desc
     puts xmlNode = form_node(start, stop, title, channel_info.fetch_row[0].to_s, chan_id, desc)
