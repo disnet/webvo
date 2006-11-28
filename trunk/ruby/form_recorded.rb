826ca270b303e9bbfb7a445698f1dec46568165b
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-28 05:19:48 +0000

    git-svn-id: http://webvo.googlecode.com/svn@276 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/form_recorded.rb
+++ trunk/ruby/form_recorded.rb
@@ -48,7 +48,7 @@ def add_size_to_xml_Node(size, xmlNode)
 end
 
 #main ------------------------------------------------------------------------------------
-  puts "Content-Type: text/xml\n\n" 
+  puts "Content-Type: text/plain\n\n" 
   cgi = CGI.new     # The CGI object is how we get the arguments 
   #manually return header and parent beginning
   puts "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<!DOCTYPE tv SYSTEM \"xmltv.dtd\">\n<tv source-info-url=\"http://labs.zap2it.com/\" source-info-name=\"TMS Data Direct Service\" generator-info-name=\"XMLTV\" generator-info-url=\"http://www.xmltv.org/\">"
@@ -66,6 +66,7 @@ rescue MysqlError => e
       dbh.close() 
     end
   else
+    puts "1"
     #look up information in directory where recorded shows are being saved
     
     #for each check against record.rb's pattern
@@ -73,9 +74,11 @@ rescue MysqlError => e
     rec_dir = Dir.new(SHOW_DIR)
     rec_array = rec_dir.entries
     rec_info = dbh.query("SELECT start, channelID FROM Recorded")  
-
+    
+    puts "2"
     #file may be there but need to compare with title in programme
       rec_info.each_hash do |recorded|
+        puts "3"
         chan_id = recorded["channelID"]
         start = recorded["start"]
         
@@ -84,6 +87,7 @@ rescue MysqlError => e
         got_programme = false
         channel_num = dbh.query("SELECT number FROM Channel WHERE (channelID = '#{chan_id}')").fetch_row[0]
         programmes.each_hash do |programme|
+          puts "4"
           if rec_array.include?(programme["title"]+programme["start"] + channel_num + ".mpg") && got_programme != false:
             f_size = File.size(programme["title"]+programme["start"] +channel_num+ ".mpg")
             puts add_size_to_xmlNode(f_size.to_i, xmlNode)
@@ -95,6 +99,7 @@ rescue MysqlError => e
           end
         end
       end
-    
+    puts "5"
     dbh.close()
   end
+  puts "6"
