8e4eb4615b58a0fb735cbc3e84d00f0cbeac5641
dsbruin <dsbruin@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-29 01:43:12 +0000

    git-svn-id: http://webvo.googlecode.com/svn@296 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/delete_recorded.rb
+++ trunk/ruby/delete_recorded.rb
@@ -5,7 +5,7 @@
 require "mysql"
 require "cgi"
 
-SERVERNAME = "localhost"
+SERVERNAME = "tvbox.homelinux.com/phpmyadmin/"
 USERNAME = "root"
 USERPASS = "csc4150"
 DBNAME = "WebVo"
@@ -29,15 +29,19 @@ end
 
 if __FILE__ == $0
 
+puts "Content-Type: text/plain\n\n" 
+
 begin
   cgi = CGI.new     # The CGI object is how we get the arguments 
-  
+  puts "Input taken"
+
 #checks for 1 argument
   error_if_not_equal(cgi.keys.length(), 1, "Needs one argument")
   error_if_not_equal(cgi.has_key?(PROG_ID), true, "Needs Programme ID")
-
+  puts "checked for 1 arg"
 #get argument
   prog_id =  cgi[PROG_ID][0]
+  puts "prog_id returned"
   error_if_not_equal(prog_id.length > LENGTH_OF_DATE_TIME, true, "Needs a Channel ID")
   date_time = prog_id[(prog_id.length-LENGTH_OF_DATE_TIME).to_i..(prog_id.length-1).to_i]
   chan_id = prog_id[0..(prog_id.length-LENGTH_OF_DATE_TIME-1).to_i]
@@ -46,40 +50,50 @@ begin
   dbh = databaseconnect()
   shownameres = dbh.query("SELECT Title FROM Programme WHERE (ChannelID='#{chan_id}'AND START= '#{date_time}')")
   showname = shownameres.fetch_row
+  puts "show name acquired"
     
 #check the hard drive for the show to be deleted
-  onHD = system("ls #{VIDEO_PATH}/#{showname}.mpg")
-     
+  onHD = system("ls #{VIDEO_PATH}/#{showname}.mpg")     
 #if does not exist, return error
   if onHD.gets != "#{VIDEO_PATH}/#{showname}.mpg"
      puts "Show does not need to be deleted"
      exit
 #if it does,remove it from recording, programme
   else
+     puts "Show located"
      dbh.query("DELETE FROM Recorded Where ShowName = '#{showname}'")
      puts "Removed from Recorded"
      dbh.query("DELETE FROM Programme Where ShowName = '#{showname}'")
      puts "Removed from Programme"
+     dbh.close()
+
 #remove from hard drive
      #need to locate all fragments as well
      lastchar = showname[showname.length-4]
+
 #remove first fragment
      deletefromHD = IO.popen ("rm #{VIDEO_PATH}/#{showname}.mpg")
      puts deletefromHD
+
 #check for more fragments
-     while (deletefromHD != )
+     while (!deletefromHD.nil?)
         lastchar += 1
         puts lastchar
+     #reinsert into title string
         showname[lastcharnum-4] = lastchar
-        deletefromHD = system ("rm #{VIDEO_PATH}/#{showname}.mpg")
-             
+     #see if the show exists
+        checkforfrags = system("ls #{VIDEO_PATH}/#{showname}.mpg")
+     #if it doesn't, get out of loop
+        if checkforfrags.nil?
+          deletefromHD.close()
+          break
+#if it does, delete from HD, look for another fragment
+        else  
+          deletefromHD = system ("rm #{VIDEO_PATH}/#{showname}.mpg")
+        end  
+    
      end
-
-#reinsert into title string
-
-
-     checkforfrags = system("ls #{VIDEO_PATH}/#{showname}.mpg")
-     
+#all done
      puts "Removed from hard drive"
   end
 end
