5590bc7d06c4582f827f49861d5005b6c303ba0f
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-29 05:29:01 +0000

    git-svn-id: http://webvo.googlecode.com/svn@364 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/form_recording.rb
+++ trunk/ruby/form_recording.rb
@@ -71,8 +71,7 @@ end
       show = dbh.query("SELECT xmlNode FROM Programme WHERE (start='#{start}' AND channelID='#{chan_id}')")
       show_info = show.fetch_row
       if show_info != nil:
-        show_info.to_s.gsub!("_*_", "'")
-        puts show_info.gsub("&", "&#38;")
+        puts show_info.to_s.gsub("_*_", "'")
       else
         dbh.close()
         error_if_not_equal(true, false, "recording programme not in programme "+ start.to_s + " " + chan_id.to_s)
