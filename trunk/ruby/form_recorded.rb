3373781cb12d11c7c89f568378467c003ba766b7
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-29 03:45:14 +0000

    git-svn-id: http://webvo.googlecode.com/svn@347 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/form_recorded.rb
+++ trunk/ruby/form_recorded.rb
@@ -93,7 +93,7 @@ rescue MysqlError => e
           puts "true dat"
           f_size = File.size(show_name + "-0" + ".mpg")
           frag_num = 1
-          while rec_array.include?(show_name + "-" + frag_num.to_s + ".mpg")
+          until rec_array.include?(show_name + "-" + frag_num.to_s + ".mpg") == false:
             puts "*"
             f_size = f_size + File.size(show_name + "-" + frag_num + ".mpg")
             frag_num = frag_num + 1
