39c2156e57f8dea2b5a4aae2dda18764cf0d084c
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-30 02:48:12 +0000

    git-svn-id: http://webvo.googlecode.com/svn@397 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/form_recorded.rb
+++ trunk/ruby/form_recorded.rb
@@ -74,44 +74,29 @@ rescue MysqlError => e
     rec_dir = Dir.new(SHOW_DIR)
     Dir.chdir(SHOW_DIR)
     rec_array = rec_dir.entries
-    
-    
-    rec_array.each do |thing|
-      puts thing
-    end
-    
     rec_info = dbh.query("SELECT start, channelID, ShowName FROM Recorded")  
     
     #file may be there but need to compare with title in programme
       rec_info.each_hash do |recorded|
-        puts chan_id = recorded["channelID"]
-        puts start = recorded["start"]
-        puts show_name = recorded["ShowName"]
+        chan_id = recorded["channelID"]
+        start = recorded["start"]
+        show_name = recorded["ShowName"]
         
         #look up programme that matches start date and channelID to later compare with title
         programmes = dbh.query("SELECT xmlNode FROM Programme WHERE (start = '#{start}' AND channelID = '#{chan_id}')")
         
         
         if rec_array.include?(show_name + "-0"+".mpg"):
-          puts "true dat"
-          puts "#{show_name}-0.mpg"
-          puts f_size = File.size("#{show_name}-0.mpg")
+          f_size = File.size("#{show_name}-0.mpg")
           frag_num = 1
-          puts show_name + "-" + frag_num.to_s + ".mpg"
           while rec_array.include?(show_name + "-" + frag_num.to_s + ".mpg") == true
-            puts "*"
             f_size = f_size + File.size("#{show_name}-#{frag_num.to_s}.mpg")
             frag_num = frag_num + 1
           end
-          puts "made it past the while"
           programme = programmes.fetch_row
           if programme != nil:
-            puts "in conditional"
-            puts programme.to_s
             puts add_size_to_xml_Node(f_size.to_i, programme.to_s)
-            puts "interesting"
           end
-          puts "other world"
         else
           #duplicate in db or programme file not in directory either way entry should be deleted
           #dbh.query("DELETE FROM Programme WHERE (channelID=('#{chan_id}') AND start = '#{start}')")
