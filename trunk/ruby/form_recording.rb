01fd4484d3dbb1abb2a59434d1e476fc6c25ea54
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-11-30 03:20:48 +0000

    git-svn-id: http://webvo.googlecode.com/svn@405 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/form_recording.rb
+++ trunk/ruby/form_recording.rb
@@ -73,7 +73,7 @@ end
       if show_info != nil:
         xml_node = show_info.to_s
         xml_node.gsub!("_*_", "'")
-        xml_node.gsub!("&", "&#38;")
+        #xml_node.gsub!("&", "&#38;")
         puts xml_node
       else
         dbh.close()
