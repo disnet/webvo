72d275586624638da6d4d5f9b3a7a606df0d1b09
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-12-01 23:33:17 +0000

    git-svn-id: http://webvo.googlecode.com/svn@435 7c433a8e-ac21-0410-9248-3fa6fae40c50

---
+++ trunk/ruby/from_cgi.rb
@@ -0,0 +1,11 @@
+require 'cgi'
+
+  puts "Content-Type: text/plain\n\n" 
+  
+  cgi = CGI.new     # The CGI object is how we get the arguments 
+  
+  puts "calling faux_record.rb"
+  
+  system("ruby faux_record.rb")
+  
+  puts "done"
