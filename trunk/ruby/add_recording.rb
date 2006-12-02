1d0a445e3df193062ffc36da5364919990dffcdf
mollyjo <mollyjo@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-12-02 02:16:01 +0000

    git-svn-id: http://webvo.googlecode.com/svn@469 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/add_recording.rb
+++ trunk/ruby/add_recording.rb
@@ -130,172 +130,172 @@ end
   cgi = CGI.new     # The CGI object is how we get the arguments 
   
 #checks for 1 argument
-  error_if_not_equal(cgi.keys.length(), 1, "Needs one argument")
-  error_if_not_equal(cgi.has_key?(PROG_ID), true, "Needs Programme ID")
+#  error_if_not_equal(cgi.keys.length(), 1, "Needs one argument")
+#  error_if_not_equal(cgi.has_key?(PROG_ID), true, "Needs Programme ID")
 
 #get argument
-  prog_id =  cgi[PROG_ID][0]
+#  prog_id =  cgi[PROG_ID][0]
 
-  error_if_not_equal(prog_id.length > LENGTH_OF_DATE_TIME, true, "Needs a Channel ID")
+#  error_if_not_equal(prog_id.length > LENGTH_OF_DATE_TIME, true, "Needs a Channel ID")
 
-  date_time = prog_id[(prog_id.length-LENGTH_OF_DATE_TIME).to_i..(prog_id.length-1).to_i]
-  chan_id = prog_id[0..(prog_id.length-LENGTH_OF_DATE_TIME-1).to_i]
+#  date_time = prog_id[(prog_id.length-LENGTH_OF_DATE_TIME).to_i..(prog_id.length-1).to_i]
+#  chan_id = prog_id[0..(prog_id.length-LENGTH_OF_DATE_TIME-1).to_i]
   
-  start_date = date_time[0..7]
-  start_time = date_time[8..13]
+#  start_date = date_time[0..7]
+#  start_time = date_time[8..13]
 
 #error checking
   #Check if times are valid
-  error_if_not_equal(start_date.to_i.to_s == start_date, true, "the date time needs to have only numbers in it")
-  error_if_not_equal(start_time.to_i < 240000, true, "Time must be millitary time")
-  error_if_not_equal(start_time[2..3].to_i < 60 , true, "Minutes must be less than 60")
+#  error_if_not_equal(start_date.to_i.to_s == start_date, true, "the date time needs to have only numbers in it")
+#  error_if_not_equal(start_time.to_i < 240000, true, "Time must be millitary time")
+#  error_if_not_equal(start_time[2..3].to_i < 60 , true, "Minutes must be less than 60")
   
   #Check if dates are valid
-  error_if_not_equal(start_date[4..5].to_i <= 12 , true, "Starting month must be <= to 12")
-  error_if_not_equal(start_date[6..7].to_i <= 31, true, "Starting month error < 31") 
+#  error_if_not_equal(start_date[4..5].to_i <= 12 , true, "Starting month must be <= to 12")
+#  error_if_not_equal(start_date[6..7].to_i <= 31, true, "Starting month error < 31") 
 
 #get programme from info.xml
-  error_if_not_equal(file_available(XML_FILE_NAME), true, "Source .xml file not in directory")
-  xml = XML::Document.file(XML_FILE_NAME)
+#  error_if_not_equal(file_available(XML_FILE_NAME), true, "Source .xml file not in directory")
+#  xml = XML::Document.file(XML_FILE_NAME)
 
-  error_if_not_equal(freespace(), true, "not enough room on server")
-  got_programme = true
+#  error_if_not_equal(freespace(), true, "not enough room on server")
+#  got_programme = true
   
 
 #These all filled with ' ' just incase they are not filled below
-  start = ' '
-  stop = ' '
-  xmlNode = ' '
-  title = ' '
-  desc = ' '
+#  start = ' '
+#  stop = ' '
+#  xmlNode = ' '
+#  title = ' '
+#  desc = ' '
   
   #getting information to put in xmlNode
   #reforming node to look like
-  xml.find("programme").each do |e|
-    if (e["channel"] == chan_id && e["start"][0..(LENGTH_OF_DATE_TIME-1).to_i] == date_time):
+#  xml.find("programme").each do |e|
+#    if (e["channel"] == chan_id && e["start"][0..(LENGTH_OF_DATE_TIME-1).to_i] == date_time):
       #get channel id, start time, stop time, title, and all xml information
       #channel id -> chan_id
       #start time -> start
       #stop time -> stop
       #title -> title
       
-      error_if_not_equal(got_programme, true, "two or more programmes match that program ID")
+#      error_if_not_equal(got_programme, true, "two or more programmes match that program ID")
       
-      start = e["start"][0..LENGTH_OF_DATE_TIME-1]
-      stop = e["stop"][0..LENGTH_OF_DATE_TIME-1]
+#      start = e["start"][0..LENGTH_OF_DATE_TIME-1]
+#      stop = e["stop"][0..LENGTH_OF_DATE_TIME-1]
       
       #make sure that stop is after current date time
-      today = format_date(DateTime.now)
-      itoday = today.to_i
-      error_if_not_equal(itoday < stop.to_i, true, "today is #{today} and your requested show ends in the past at #{stop}")
-      error_if_not_equal(e.child?, true, "programme to add doesn't have needed information")
-      c = e.child
+#      today = format_date(DateTime.now)
+#      itoday = today.to_i
+#      error_if_not_equal(itoday < stop.to_i, true, "today is #{today} and your requested show ends in the past at #{stop}")
+#      error_if_not_equal(e.child?, true, "programme to add doesn't have needed information")
+#      c = e.child
       
-      keep_looping = true #varible to do a do-while
+#      keep_looping = true #varible to do a do-while
       
       #gets the title
-      while keep_looping == true:
-        if c.name == "title":
-          title = c.content
-          need_title = false
-        end  
-        if c.name == "desc":
-          desc = c.content
-        end
+#      while keep_looping == true:
+#        if c.name == "title":
+#          title = c.content
+#          need_title = false
+#        end  
+#        if c.name == "desc":
+#          desc = c.content
+#        end
         #if there needs to be more things code will need to be added here
-        if c.next?:
-            c = c.next
-        else
-          keep_looping = false
-        end
-      end
-      error_if_not_equal(need_title, false, "programme doesn't have a title")
-      got_programme = false
-    end
-  end
+#        if c.next?:
+#            c = c.next
+#        else
+#          keep_looping = false
+#        end
+#      end
+#      error_if_not_equal(need_title, false, "programme doesn't have a title")
+#      got_programme = false
+#    end
+#  end
   
-  error_if_not_equal(got_programme, false, "requested show not in source listings") 
+#  error_if_not_equal(got_programme, false, "requested show not in source listings") 
   
   #get the integer versions of start and stop to use to check if the programme
   #to be added is at the same time as a current show.
-  istart = start.to_i
-  istop = stop.to_i
+#  istart = start.to_i
+#  istop = stop.to_i
   
   #connect to database
-  begin
-  dbh = Mysql.real_connect("#{SERVERNAME}","#{USERNAME}","#{USERPASS}","#{DBNAME}")
+#  begin
+#  dbh = Mysql.real_connect("#{SERVERNAME}","#{USERNAME}","#{USERPASS}","#{DBNAME}")
 #  if gets an error (can't connect)
-  rescue MysqlError => e
-      error_if_not_equal(false,true, "Error code: " + e.errno + " " + e.error + "\n")
-    if dbh.nil? == false
+#  rescue MysqlError => e
+#      error_if_not_equal(false,true, "Error code: " + e.errno + " " + e.error + "\n")
+#    if dbh.nil? == false
       #close the database
-      dbh.close() 
-    end
-  else
+#      dbh.close() 
+#    end
+#  else
     #check and make sure that the programme isn't already there
-    presults = dbh.query("SELECT * FROM Programme WHERE (channelID = '#{chan_id}' AND start = '#{start}')")
-    rresults = dbh.query("SELECT * FROM Recording WHERE (channelID ='chan_id}' AND start = '#{start}')")
+#    presults = dbh.query("SELECT * FROM Programme WHERE (channelID = '#{chan_id}' AND start = '#{start}')")
+#    rresults = dbh.query("SELECT * FROM Recording WHERE (channelID ='chan_id}' AND start = '#{start}')")
     
-    if presults.fetch_row != nil:
-      dbh.close()
-      error_if_not_equal(false, true, "show already added")
-    end
+#    if presults.fetch_row != nil:
+#      dbh.close()
+#      error_if_not_equal(false, true, "show already added")
+#    end
    
     #check to see if there is a programme during the same time.
-    allpresults = dbh.query("SELECT start, stop, channelID, title FROM Programme ORDER BY start")
+#    allpresults = dbh.query("SELECT start, stop, channelID, title FROM Programme ORDER BY start")
     
     #loop through the results and check if they are during the same time
-    allpresults.each_hash do |row|
-      qstart = row["start"].to_i
-      qstop = row["stop"].to_i
+#    allpresults.each_hash do |row|
+#      qstart = row["start"].to_i
+#      qstop = row["stop"].to_i
       #possible locations of programmes
-      begins_before = qstop > istart && qstop <= istop && qstart <= istop && qstart < istart
-      ends_after = qstart >= istart && qstop > istop && qstart < istop
-      occurs_during = qstart >= istart && qstop <= istop
-      occurs_around = (qstart <= istart && qstop >= istop)
+#      begins_before = qstop > istart && qstop <= istop && qstart <= istop && qstart < istart
+#      ends_after = qstart >= istart && qstop > istop && qstart < istop
+#      occurs_during = qstart >= istart && qstop <= istop
+#      occurs_around = (qstart <= istart && qstop >= istop)
       
       #if programme to add is during a programme that is already in the database
-      if begins_before || ends_after || occurs_during || occurs_around:
-        schan_id = row["channelID"]
+#      if begins_before || ends_after || occurs_during || occurs_around:
+#        schan_id = row["channelID"]
         #see if this programme is in recording, if so then error out
-        show_in_recording = dbh.query("SELECT start FROM Recording WHERE (channelID ='#{schan_id}' AND start = '#{qstart.to_s}')")
-        recording_res = show_in_recording.fetch_row
-        if recording_res != nil:
+#        show_in_recording = dbh.query("SELECT start FROM Recording WHERE (channelID ='#{schan_id}' AND start = '#{qstart.to_s}')")
+#        recording_res = show_in_recording.fetch_row
+#        if recording_res != nil:
           
-          title_with_spaces = row["title"].gsub(/["_"]/," ")
+#          title_with_spaces = row["title"].gsub(/["_"]/," ")
           
-          dbh.close()
+#          dbh.close()
           
-          error_if_not_equal(true, false, "Requested show occurs during: " + title_with_spaces)
+#          error_if_not_equal(true, false, "Requested show occurs during: " + title_with_spaces)
           
-        end
+#        end
         
-      end
-    end
+#      end
+#    end
     
     #look up channel number to include in xmlNode
-    channel_info = dbh.query("SELECT number FROM Channel WHERE channelID ='#{chan_id}' LIMIT 1")
-    channel_num = channel_info.fetch_row
-    if channel_num == nil:
-      dbh.close()
-      error_if_not_equal(true, false, "channel from requested show not in database")
-    end
+#    channel_info = dbh.query("SELECT number FROM Channel WHERE channelID ='#{chan_id}' LIMIT 1")
+#    channel_num = channel_info.fetch_row
+#    if channel_num == nil:
+#      dbh.close()
+#      error_if_not_equal(true, false, "channel from requested show not in database")
+#    end
 
-    xmlNode = form_node(start, stop, title, channel_num, chan_id, desc).to_s
+#    xmlNode = form_node(start, stop, title, channel_num, chan_id, desc).to_s
     #send information to programme's table 
       #change data a bit to get it not to error when put in the database
-    xmlNode.gsub!(/["'"]/, "_*_")
-    xmlNode.gsub!("&", "&#38;")
-    title = title.gsub(/[" "]/,"_")
-    dbh.query("INSERT INTO Programme (channelID, start, stop, title, xmlNode) VALUES ('#{chan_id}', '#{start}','#{stop}','#{title}','#{xmlNode}')")
-    if rresults.fetch_row == nil:
+#    xmlNode.gsub!(/["'"]/, "_*_")
+#    xmlNode.gsub!("&", "&#38;")
+#    title = title.gsub(/[" "]/,"_")
+#    dbh.query("INSERT INTO Programme (channelID, start, stop, title, xmlNode) VALUES ('#{chan_id}', '#{start}','#{stop}','#{title}','#{xmlNode}')")
+#    if rresults.fetch_row == nil:
       #send information to recording table
-      dbh.query("INSERT INTO Recording (channelID, start) VALUES ('#{chan_id}', '#{start}')")
-    end
+#      dbh.query("INSERT INTO Recording (channelID, start) VALUES ('#{chan_id}', '#{start}')")
+#    end
     #close the database
-    dbh.close()
-  end
-  puts "<success>#{prog_id}</success>"
+#    dbh.close()
+#  end
+#  puts "<success>#{prog_id}</success>"
 
   #call record.rb
 
@@ -304,7 +304,6 @@ end
   end
   
   cgi.close()
-  Process.detach(pid)
 
   
   #log = File.open("add_recoringlog.txt","a")
