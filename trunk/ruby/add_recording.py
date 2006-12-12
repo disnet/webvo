b34cc0b8df98f39af48ad51c3de76cd2d8a18e9a
tim.disney <tim.disney@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2006-12-12 08:15:10 +0000

    git-svn-id: http://webvo.googlecode.com/svn@507 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/add_recording.py
+++ trunk/ruby/add_recording.py
@@ -3,8 +3,6 @@ import cgi,sys,os
 
 print "Content-Type:text/xml\n\n"
 
-f = open('pylog.txt','w')
-
 form = cgi.FieldStorage()
 if not form.has_key("prog_id"):
     print "<error>must include programme ID</error>"
@@ -15,6 +13,4 @@ progID = form["prog_id"].value
 t = os.popen('ruby add_recording.rb ' + progID, 'r')
 
 out = t.read() 
-f.write(out)
-
 print out
