aaa95e8ac716245610fb274262d88771bbdd4cc8
tim.disney <tim.disney@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2007-03-21 22:28:54 +0000

    git-svn-id: http://webvo.googlecode.com/svn@529 7c433a8e-ac21-0410-9248-3fa6fae40c50

---
+++ branches/demo/ruby/delete_recorded.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import cgi,sys,os
+
+#print "Content-Type:text/xml\n\n"
+
+form = cgi.FieldStorage()
+if not form.has_key("prog_id"):
+    print "<error>must include programme ID</error>"
+    sys.exit()
+
+progID = form["prog_id"].value
+
+t = os.popen('ruby delete_recorded.rb ' + progID, 'r')
+
+print t.read()
