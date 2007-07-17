db9e73de4325b9e5c1f91b1880ad19ee19a8816a
mr.david.hopkins <mr.david.hopkins@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2007-07-17 03:06:54 +0000

    Added record startup script.  It is setup from ruby/config.rb, which must be run as root now.
    git-svn-id: http://webvo.googlecode.com/svn@639 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/config.rb
+++ trunk/ruby/config.rb
@@ -4,10 +4,17 @@ require 'mysql'
 require 'util'
 
 DB_FILE = "Webvo.sql"
+STARTUP_FILE = "webvo"
+SCRIPT_LOCATION = "/etc/init.d/webvo"
+SCRIPT_START = "/etc/rc2.d/S80webvo"
+SCRIPT_STOP = "/etc/rc1.d/K10webvo"
+
+#todo: dynamically define the webvo dir in the startup script?
 
 dbh = Mysql.real_connect(
     "#{SERVERNAME}","#{USERNAME}","#{USERPASS}",nil)
 
+# setup database
 unless dbh.list_dbs.include?(DBNAME)
     dbh.query("CREATE DATABASE #{DBNAME}")
     dbh.select_db(DBNAME)
@@ -15,3 +22,13 @@ unless dbh.list_dbs.include?(DBNAME)
     dbh.query(dbSchema){}
     #system("mysql --user=#{USERNAME} --password=#{USERPASS} #{DBNAME} < #{CONFIG_PATH+DB_FILE}")
 end
+dbh.close
+
+# setup record to autorun at computer boot
+start_script = File.open(SCRIPT_LOCATION, File::WRONLY|File::TRUNC|File::CREAT)
+start_script << File.read(CONFIG_PATH+STARTUP_FILE)
+start_script.close
+File.delete(SCRIPT_START, SCRIPT_STOP)
+File.symlink(SCRIPT_LOCATION, SCRIPT_START)
+File.symlink(SCRIPT_LOCATION, SCRIPT_STOP)
+system(SCRIPT_LOCATION + " start")
