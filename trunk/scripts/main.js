586c982a80fd211a80cdf6684e0248ddc89c4e55
tim.disney <tim.disney@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2007-09-20 06:45:55 +0000

    more work done on request handler
    still need to get scheduled and recorded to work
    
    
    git-svn-id: http://webvo.googlecode.com/svn@682 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/scripts/main.js
+++ trunk/scripts/main.js
@@ -4,37 +4,89 @@ function App(dbg){
    this.listing_data = null;
    this.scheduled_data = null;
    this.recorded_data = null;
+
+   this.search_data = new SearchData();
+
+   this.pages = [$('listingContent'), $('scheduledContent'), $('recordedContent'), $('searchContent')];
 }
 
 App.prototype = {
     init: function() {
-        Util.makeInvisible($('listingContent'));
-        Util.makeVisible($('searchContent'));
+        this.showSearch();
+        connect('btnSearchSubmit','onclick',this,'searchSubmit');
 
-        connect('btnSearchSubmit','onclick',search.submit);
-    }
-};
-
-var search = {
-    submit: function(e) {
-        var searchText = $('txtSearchTitle').value;
-        var d = loadJSONDoc('ruby/form_search.rb',{'title':searchText,'format':'new'});
-        d.addCallbacks(search.gotSearch,search.fetchFailed);
+        connect('btnListing','onclick',this,'showListing');
+        connect('btnScheduled','onclick',this,'showScheduled');
+        connect('btnRecorded','onclick',this,'showRecorded');
+        connect('btnSearch','onclick',this,'showSearch');
     },
-    gotSearch: function(req) {
-        app.search_data = req;
-        var searchTable = $('searched');
-        console.log(req);
-        searchTable.innerHTML = req.search.header;
-        for(var i = 0; i < req.search.programmes.length; i++) {
-            searchTable.innerHTML += req.search.programmes[i].html;
+    
+    searchSubmit: function(e) {
+        this.search_data.setQuery('title',$('txtSearchTitle').value);
+        this.search_data.update();
+    },
+
+    /* Display/hide pages */
+    showListing: function() {
+        this._displayPage($('listingContent')); 
+    },
+    showScheduled: function() {
+        this._displayPage($('scheduledContent')); 
+    },
+    showRecorded: function() {
+        this._displayPage($('recordedContent')); 
+    },
+    showSearch: function() {
+        this._displayPage($('searchContent')); 
+    },
+
+    _displayPage: function(elDisplay) {
+        for(var i = 0; i < this.pages.length; i++) {
+            if(this.pages[i] != elDisplay) {
+                Util.makeInvisible(this.pages[i]);
+            }
         }
-    },
-    fetchFailed: function(req) {
-        console.error("Problem retrieving search results:");
-        console.error(req);
+        Util.makeVisible(elDisplay);
     }
 };
+
+function SearchData() {
+    bindMethods(this); /* preserve `this` for callbacks */
+}
+SearchData.prototype = new JSONRequest('ruby/form_search.rb');
+
+SearchData.prototype.setQuery = function(term,val) {
+    this._query[term] = val;
+}
+
+SearchData.prototype.reqHandler = function() {
+    var searchTable = $('searched');
+    searchTable.innerHTML = this.data.search.header;
+    for(var i = 0; i < this.data.search.programmes.length; i++) {
+        searchTable.innerHTML += this.data.search.programmes[i].html;
+    }
+}
+SearchData.prototype.searchFailed = function(req) { 
+    console.error("Problem retrieving search results:");
+    console.error(req);
+}
+
+function JSONRequest(url) {
+    this._url = url;
+    this._query = {'json':'true'};
+}
+JSONRequest.prototype.update = function() {
+    var d = loadJSONDoc(this._url,this._query);
+    d.addCallbacks(this._gotRequest,this._fetchFailed);
+}
+JSONRequest.prototype._gotRequest = function(req) {
+    this.data = req;
+    this.reqHandler();
+}
+JSONRequest.prototype._fetchFailed = function(req) {
+    console.log('Abstract method...should have been implemented in child');
+}
+
 
 var app;
 window.onload = function() {
