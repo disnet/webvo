4ec21ecfff71fbe8d9c4d47812330f1375860b80
tim.disney <tim.disney@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2007-11-04 08:29:47 +0000

    work done on adder
    git-svn-id: http://webvo.googlecode.com/svn@720 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/scripts/main.js
+++ trunk/scripts/main.js
@@ -1,6 +1,6 @@
 function App(dbg){
     this._dbg = dbg; 
-
+    this.adder = new Adder()
     this.listing_data = new ListingData('ruby/form_listing.rb');
 
     this.scheduled_table = new InfoTable('scheduled','ruby/form_scheduled.rb','ruby/add_recording.rb');
@@ -16,6 +16,7 @@ App.prototype = {
     init: function() {
         this.showSearch();
         connect('btnSearchSubmit','onclick',this,'searchSubmit');
+        connect('btnSearchRecord','onclick',this,'searchAddRecording');
 
         connect('btnListing','onclick',this,'showListing');
         connect('btnScheduled','onclick',this,'showScheduled');
@@ -25,6 +26,12 @@ App.prototype = {
     
     searchSubmit: function(e) {
         this.search_table.update('json=true&title=' + $('txtSearchTitle').value);
+    },
+
+    searchAddRecording: function(e) {
+        var sel = filter(function(el){ return el.checked; }, document.getElementsByName('searchCheck'));
+        console.log(sel);
+        this.adder.add(sel.value);
     },
 
     /* Display/hide pages */
@@ -54,6 +61,23 @@ App.prototype = {
     }
 };
 
+function Adder() {
+   bindMethods(this); 
+}
+
+Adder.prototype = {
+    add: function(id) {
+        var d = loadJSONDoc('ruby/add_recording.rb?json=true&prog_id=' + id);
+        this._deffered = d;
+        d.addCallbacks(this._gotRequest,this._fetchFailed);
+    },
+    _gotRequest: function(req) {
+        console.log(req);
+    },
+    _fetchFailed: function(req) {
+        console.log(req);
+    }
+};
 var app;
 window.onload = function() {
     app = new App(true);
