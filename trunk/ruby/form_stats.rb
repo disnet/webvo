77c69e5828d015bb07a9f1082913babc04564d42
mr.david.hopkins <mr.david.hopkins@7c433a8e-ac21-0410-9248-3fa6fae40c50>
2007-11-11 09:59:25 +0000

    form_stats.rb now returns json formatted data
    git-svn-id: http://webvo.googlecode.com/svn@736 7c433a8e-ac21-0410-9248-3fa6fae40c50

--- trunk/ruby/form_stats.rb
+++ trunk/ruby/form_stats.rb
@@ -2,29 +2,35 @@
 
 require 'util'
 
-puts XML_HEADER
+DATE_FORMAT_STRING = "%Y-%m-%d"
+
+puts JSON_HEADER
+
+retstr = "{ 'type': 'stats'"
 
 stop_day = databasequery("SELECT DATE_FORMAT(stop,'#{DATE_FORMAT_STRING}') FROM Programme ORDER BY stop DESC LIMIT 1").fetch_row
 #start_day = Time.now.strftime(DATE_FORMAT_STRING)
 start_day = databasequery("SELECT DATE_FORMAT(start,'#{DATE_FORMAT_STRING}') FROM Programme ORDER BY start ASC LIMIT 1").fetch_row
-puts "<programme_date_range start='"+ start_day.to_s + "' stop='" + stop_day.to_s + "'></programme_date_range>\n"
-
+retstr += ",\n'programme_date_range': {'start': '"+ start_day.to_s + "', 'stop': '" + stop_day.to_s + "'}"
 space = freespace
 
-puts "<space>"
-puts "<available>" + space['available'] + "</available>"
-puts "<used>" + space['used'] + "</used>"
-puts "<total>" + space['total'] + "</total>"
-puts "</space>"
+retstr += ",\n'space': {" +
+    "'available': '#{space['available']}', " +
+    "'used': '#{space['used']}', " +
+    "'total': '#{space['total']}' }"
+
 
 recording = getNextShow
 
 unless recording.nil?
-    puts "<recording>"
-    puts recording.xmlNode
-    puts "</recording>"
+    retstr += ",\n'recording': {" +
+        "'id': '#{recording.channelID+recording.start_xml}', " +
+        "'channel': '#{recording.channel}', " +
+        "'filename': '#{recording.filename.to_s.gsub(/'/,"&#39;")}' }"
 end
 
-puts "<datetime>" + Time.new.strftime(DATE_TIME_FORMAT_RUBY_XML) + "</datetime>"
+retstr += ",\n'datetime': '#{Time.new.strftime(DATE_TIME_FORMAT_RUBY_XML)}'"
 
-puts XML_FOOTER
+retstr += "\n}"
+
+puts retstr
